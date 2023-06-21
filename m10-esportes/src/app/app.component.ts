63129081aa603f97a35247628e29a88a11cc3fab
luizamonteiro <maria.monteiro@parnamirim.rn.gov.br>
2023-06-21 14:51:11 -0300

    git pull.

--- m10-esportes/src/app/app.component.ts
+++ m10-esportes/src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
-import { ModalService } from './support/components/modal/service/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'm10-esportes';
-  constructor(private modalService: ModalService){-}
-
-  openModal(title: string, description: string) {
-    this.modalService
-      .open(AppComponent, {})
-      .subscribe();
-  }
 }
--- m10-esportes/src/app/app.module.ts
+++ m10-esportes/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ModalComponent } from './support/components/modal/modal.component';
+import { NavBarComponent } from './support/components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModalComponent
   ],
   imports: [
+    NavBarComponent,
     BrowserModule,
     AppRoutingModule
   ],

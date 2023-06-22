230d058f1fad1de9d41c3059ca3f30c1cafa5f1e
luizamonteiro <maria.monteiro@parnamirim.rn.gov.br>
2023-06-22 08:15:59 -0300

    #7 | refacting

--- m10-esportes/src/app/app.component.ts
+++ m10-esportes/src/app/app.component.ts
@@ -1,33 +1,8 @@
-import { ModalService } from './support/components/modal/service/modal.service';
 import { Component } from '@angular/core';
-import { TeamModalComponent } from './support/components/team-modal/team-modal.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
-  title = 'm10-esportes';
-  name = 'prof. wladimir januário';
-  description =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis vehicula fermentum.';
-  categories = [
-    { slug: '/assets/icons/fut7.svg', name: 'Fut7' },
-    { slug: '/assets/icons/personal.svg', name: 'Personal Soccer' },
-  ];
-  image = '';
-
-  constructor(private modalService: ModalService) {}
-
-  openModal() {
-    this.modalService
-      .open(TeamModalComponent, {
-        name: this.name,
-        description: this.description,
-        categories: this.categories,
-        image: this.image,
-      })
-      .subscribe();
-  }
-}
+export class AppComponent {}
--- m10-esportes/src/app/app.module.ts
+++ m10-esportes/src/app/app.module.ts
@@ -4,19 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './support/components/nav-bar/nav-bar.component';
-import { TeamModalComponent } from './support/components/team-modal/team-modal.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    TeamModalComponent,
-    NavBarComponent,
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [NavBarComponent, BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

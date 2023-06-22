dc520eafb270fa3aac37645af5220de751f26f80
luizamonteiro <maria.monteiro@parnamirim.rn.gov.br>
2023-06-22 15:17:27 -0300

    #13 | bug: codes replaced.

--- m10-esportes/src/app/app-routing.module.ts
+++ m10-esportes/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'portal',
+    loadChildren: () =>
+      import('./portal/portal.module').then((m) => m.PortalModule),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
